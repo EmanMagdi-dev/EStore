575d352e626e90761d2897ceffe38194cc116660
Eman Magdi <eman.magdi.akl@gmail.com>
2022-04-13 00:54:59 +0200

    first commit

---
+++ eStore/Migrations/20220309195426_m.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eStore.Migrations
+{
+    public partial class m : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableQuantity",
+                table: "Products");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AvailableQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
